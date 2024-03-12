f6108b8b6cdb5280f3302822b1c1a6a5ad8969e0
jakubGrabski67 <kubagrabski22@gmail.com>
2024-03-12 20:17:23 +0100

    Updating backend files

--- server.js
+++ server.js
@@ -36,16 +36,11 @@ app.use('/notes', require('./routes/noteRoutes'))
 app.use('/cars', require('./routes/carRoutes'))
 app.use('/reservation', require('./routes/reservationRoutes'))
 
-app.all('*', (req, res) => {
-    res.status(404)
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found' })
-    } else {
-        res.type('txt').send('404 Not Found')
-    }
-})
+// Dodanie konfiguracji przekierowania na index.html
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use(errorHandler)
 
